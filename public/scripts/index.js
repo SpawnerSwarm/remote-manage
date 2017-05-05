b0c5f6407e5bf95343b1d96d45f8bf7f85c9adc7
crunchyintheory <crunchyintheory@users.noreply.github.com>
2017-05-05 01:21:28 -0500

    Improved console formatting

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,18 +5,21 @@ socket.on('console', function (data) {
     parseConsoleJSON(data);
 });
 socket.on('log', function (data) {
-    console.log(data);
     parseConsoleJSON(data);
 });
 function parseConsoleJSON(json) {
     if (json === null) {
         throw new Error('Unable to parse console JSON');
     } else {
-        let con = document.querySelector('.content');
+        let con = document.querySelector('.console');
         for (var i = 0; i < json.length; i++) {
             let span = document.createElement('span');
-            span.className = `console-line console-${json[i].level}`;
-            span.innerText = `[${json[i].level}] ${json[i].message}`;
+            span.className = `console-line console-${json[i].level.toLowerCase()}`;
+            span.innerText = ` ${json[i].message}`;
+            let levelSpan = document.createElement('span');
+            levelSpan.className = `console-line-level console-${json[i].level.toLowerCase()}-level`;
+            levelSpan.innerText = json[i].level.substring(0, 3);
+            span.prepend(levelSpan);
             con.appendChild(span);
         }
         if (con.childElementCount > 200) {
@@ -25,7 +28,7 @@ function parseConsoleJSON(json) {
     }
 }
 function removeChildren(len) {
-    let con = document.querySelector('.content');
+    let con = document.querySelector('.console');
     let num = con.childElementCount;
 
     for (var i = 0; i < num - len; i++) {
